86d50e4eca3595c7aba18e1cf474bfe865355aea
Michael Weinstein <michael.weinstein@ucla.edu>
2024-11-21 10:13:36 -0800

    Added some initial framework for the program

---
+++ main.py
@@ -0,0 +1,5 @@
+import rpsSupport
+
+if __name__ == "__main__":
+    while True:
+        pass # TODO: Write main game loop here
---
+++ rpsSupport/game.py
@@ -0,0 +1,5 @@
+import typing
+import random
+
+
+# TODO: Add functions that carry out the gameplay
